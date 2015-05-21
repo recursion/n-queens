22862baad9ea87b783d6342072025dd9ae65c2d4
Michael Symmes <msymmes@gmail.com>
2015-05-20 20:24:33 -0400

    Implemented minor diagonal conflict

--- src/Board.js
+++ src/Board.js
@@ -146,7 +146,15 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var rows = this.rows();
+      var diagonals = [];
+
+      _.each(rows, function(row, index) {
+        if (minorDiagonalColumnIndexAtFirstRow >= 0) {
+          diagonals.push(row[minorDiagonalColumnIndexAtFirstRow--]);
+       }
+      });
+      return this._sum(diagonals) > 1;
     },
 
     // test if any minor diagonals on this board contain conflicts
